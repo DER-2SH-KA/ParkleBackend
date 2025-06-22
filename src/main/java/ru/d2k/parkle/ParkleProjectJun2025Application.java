75bbb2ddb7639279c027e460a43030498677bf47
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-06-22 21:00:58 +0700

    Add basic project files

---
+++ src/main/java/ru/d2k/parkle/ParkleProjectJun2025Application.java
@@ -0,0 +1,13 @@
+package ru.d2k.parkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkleProjectJun2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkleProjectJun2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/d2k/parkle/ParkleProjectJun2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.d2k.parkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkleProjectJun2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
