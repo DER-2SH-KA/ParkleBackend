c118fe666ea2199e8c04ac82100818e3b906ca1a
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-06-22 21:59:34 +0700

    Add dotend-java library and disable...
    liquibase, redis and security login screen

---
+++ src/main/java/ru/d2k/parkle/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package ru.d2k.parkle.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> {
+           authorize.anyRequest().permitAll();
+        }).csrf().disable();
+
+        return http.build();
+    }
+}
