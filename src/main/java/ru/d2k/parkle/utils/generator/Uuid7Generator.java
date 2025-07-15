21b3254ef713c9615b9f0e45b564add6d48e6fda
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-16 01:39:49 +0700

    feat: Add generateNewUUID() method in Uuid7Generator

--- src/main/java/ru/d2k/parkle/utils/generator/Uuid7Generator.java
+++ src/main/java/ru/d2k/parkle/utils/generator/Uuid7Generator.java
@@ -5,10 +5,18 @@ import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
 import java.io.Serializable;
+import java.util.UUID;
 
+/** Class for generate UUID values. **/
 public class Uuid7Generator implements IdentifierGenerator {
     @Override
     public Serializable generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) {
         return Generators.timeBasedEpochRandomGenerator().generate();
     }
+
+    /**
+     * Generate new UUID by UUID V7.
+     * @return Generated UUID V7 value.
+     * **/
+    public static UUID generateNewUUID() { return Generators.timeBasedEpochRandomGenerator().generate(); }
 }
