91ae2c961463e79e6da6347f1f210f446974d6cd
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-16 21:13:40 +0700

    feat: Remove HexColor files

--- src/main/java/ru/d2k/parkle/repository/HexColorRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.d2k.parkle.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.d2k.parkle.entity.HexColor;
-
-@Repository
-public interface HexColorRepository extends JpaRepository<HexColor, Integer> {
-}
