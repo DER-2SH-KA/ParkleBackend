59f3e7adeefd2a459d5bfeeebc162f988ddc7756
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-16 22:04:01 +0700

    feat: Add UserMapper

---
+++ src/main/java/ru/d2k/parkle/utils/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package ru.d2k.parkle.utils.mapper;
+
+import org.mapstruct.*;
+import ru.d2k.parkle.dto.UserResponseDto;
+import ru.d2k.parkle.dto.UserUpdateDto;
+import ru.d2k.parkle.entity.User;
+import ru.d2k.parkle.utils.resolver.RoleResolver;
+
+@Mapper(
+        componentModel = "spring",
+        uses = {RoleMapper.class, RoleResolver.class}
+)
+public interface UserMapper {
+
+    UserResponseDto toResponseDto(User entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy =
+            NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "roleId", target = "role")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "websites", ignore = true)
+    User updateByDto(@MappingTarget User entity, UserUpdateDto dto);
+
+}
---
+++ src/main/java/ru/d2k/parkle/utils/resolver/RoleResolver.java
@@ -0,0 +1,28 @@
+package ru.d2k.parkle.utils.resolver;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.d2k.parkle.entity.Role;
+import ru.d2k.parkle.repository.RoleRepository;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class RoleResolver {
+
+    private final RoleRepository roleRepository;
+
+    @Transactional(readOnly = true)
+    public Role resolve(UUID roleId) {
+        if (roleId == null) return null;
+
+        return roleRepository.findById(roleId)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Role not found with ID: " + roleId)
+                );
+    }
+
+}
