2813f7c82aeb94bdc84edc3ea8efc6e24b543866
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-18 00:21:39 +0700

    refactor: Refactor paths of ApiPaths

--- src/main/java/ru/d2k/parkle/controller/ApiPaths.java
+++ src/main/java/ru/d2k/parkle/controller/ApiPaths.java
@@ -1,7 +1,12 @@
 package ru.d2k.parkle.controller;
 
 public interface ApiPaths {
-    String ROLE_API = "/api/roles/";
-    String WEBSITE_API = "/api/websites/";
-    String AUTH_API = "/api/auth/";
+    String API = "/api/";
+
+    String ROLE_API = API + "roles/";
+    String USER_API = API + "users/";
+    String WEBSITE_API = API + "websites/";
+    String AUTH_API = API + "auth/";
+    String AUTH_LOGIN_API = AUTH_API + "login/";
+    String AUTH_REGISTRATION_API = AUTH_API + "registration/";
 }
