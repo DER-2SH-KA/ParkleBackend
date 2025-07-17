c4094bef635b1b813374b2813105b21b5ad16379
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-18 00:35:32 +0700

    feat: Add main methods for UserService (Find and create)

--- src/main/java/ru/d2k/parkle/service/rest/UserService.java
+++ src/main/java/ru/d2k/parkle/service/rest/UserService.java
@@ -1,15 +1,75 @@
 package ru.d2k.parkle.service.rest;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.d2k.parkle.dto.UserCreateDto;
+import ru.d2k.parkle.dto.UserResponseDto;
+import ru.d2k.parkle.entity.Role;
+import ru.d2k.parkle.entity.User;
+import ru.d2k.parkle.repository.RoleRepository;
 import ru.d2k.parkle.repository.UserRepository;
+import ru.d2k.parkle.utils.mapper.UserMapper;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
-    @Autowired
-    private final UserRepository USER_REPOSITORY;
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    /**
+     * Return all users from DB as DTO.
+     * @return Set of dto.
+     * **/
+    @Transactional(readOnly = true)
+    public Set<UserResponseDto> findUsers() {
+        log.info("Finding all users...");
+
+        Set<UserResponseDto> dtos = userRepository.findAll().stream()
+                .map(userMapper::toResponseDto)
+                .collect(Collectors.toSet());
+
+        log.info("Users was founded: {}", dtos.size());
+        return dtos;
+    }
+
+    /**
+     * Return user by ID as DTO.
+     * @param id ID of user.
+     * @return {@link UserResponseDto} dto.
+     * **/
+    @Transactional(readOnly = true)
+    public UserResponseDto findUserById(UUID id) {
+        log.info("Finding user by ID: {}...", id);
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("User was not found with ID: " + id)
+                );
+
+        log.info("User with ID = {} was founded", id);
+        return userMapper.toResponseDto(user);
+    }
+
+    @Transactional
+    public UserResponseDto createUser(UserCreateDto dto) {
+        log.info("Creating user: {}...", dto.toString());
+
+        Role role = roleRepository.findById(dto.getRoleId()).orElseThrow(() ->
+                new EntityNotFoundException("Role was not found with ID: " + dto.getRoleId())
+        );
+        User user = User.create(role, dto.getLogin(), dto.getEmail(), dto.getPassword());
+        user = userRepository.save(user);
+
+        log.info("User was created: {}", user);
+        return userMapper.toResponseDto(user);
+    }
 }
