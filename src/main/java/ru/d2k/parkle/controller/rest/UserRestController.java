6e0af91e24e773c8c6481f9afa74a217fcaf57a5
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-18 11:49:38 +0700

    feat: Add CRUD to UserRestController

--- src/main/java/ru/d2k/parkle/controller/rest/UserRestController.java
+++ src/main/java/ru/d2k/parkle/controller/rest/UserRestController.java
@@ -1,16 +1,19 @@
 package ru.d2k.parkle.controller.rest;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.d2k.parkle.controller.ApiPaths;
 import ru.d2k.parkle.dto.UserCreateDto;
 import ru.d2k.parkle.dto.UserResponseDto;
+import ru.d2k.parkle.dto.UserUpdateDto;
 import ru.d2k.parkle.service.rest.UserService;
 
 import java.util.UUID;
 import java.util.Set;
 
+@Slf4j
 @RestController
 @RequestMapping(ApiPaths.USER_API)
 @RequiredArgsConstructor
@@ -19,23 +22,51 @@ public class UserRestController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> findUserById(@PathVariable UUID id) {
-        return ResponseEntity.ok(
-                userService.findUserById(id)
-        );
+        log.info("Given GET request to return user by ID: {}", id);
+
+        UserResponseDto responseDto = userService.findUserById(id);
+
+        log.info("GET request was served");
+        return ResponseEntity.ok( responseDto );
     }
 
     @GetMapping
     public ResponseEntity<Set<UserResponseDto>> findUsers() {
-        return ResponseEntity.ok(
-                userService.findUsers()
-        );
+        log.info("Given GET request to return all users");
+
+        Set<UserResponseDto> userResponseDtos = userService.findUsers();
+
+        log.info("GET request was served");
+        return ResponseEntity.ok( userResponseDtos );
     }
 
     @PostMapping("/new")
     public ResponseEntity<UserResponseDto> createUser(@RequestBody UserCreateDto dto) {
-        return ResponseEntity.ok(
-                userService.createUser(dto)
-        );
+        log.info("Given POST user request with DTO: {}", dto);
+
+        UserResponseDto responseDto = userService.createUser(dto);
+
+        log.info("POST request was served");
+        return ResponseEntity.ok(responseDto);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUserById(@PathVariable UUID id, @RequestBody UserUpdateDto dto) {
+        log.info("Given PUT user request with ID: {}", id);
+
+        UserResponseDto responseDto = userService.updateUser(id, dto);
+
+        log.info("PUT request was served");
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable UUID id) {
+        log.info("Given DELETE user request by ID: {}", id);
+
+        userService.deleteUser(id);
+
+        log.info("DELETE request was served");
+        return ResponseEntity.ok().build();
+    }
 }
