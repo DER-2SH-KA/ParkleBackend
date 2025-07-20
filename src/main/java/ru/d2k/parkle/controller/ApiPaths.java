0962014c866b3512b13959749f656372855c20e7
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-20 14:49:04 +0700

    refactor: Delete AUTH_LOGIN_API and AUTH_REGISTRATION_API variables. Replace them on AUTH_API variable

--- src/main/java/ru/d2k/parkle/controller/ApiPaths.java
+++ src/main/java/ru/d2k/parkle/controller/ApiPaths.java
@@ -4,6 +4,5 @@ public interface ApiPaths {
     String ROLE_API = "/api/roles/";
     String USER_API = "/api/users/";
     String WEBSITE_API = "/api/websites/";
-    String AUTH_LOGIN_API = "/api/auth/login/";
-    String AUTH_REGISTRATION_API = "/api/auth/registration/";
+    String AUTH_API = "/api/auth/";
 }
