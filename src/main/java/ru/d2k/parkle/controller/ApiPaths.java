55121302587a1bd51a1aa4e8482d18de57c562b4
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-20 14:47:09 +0700

    refactor: Simplify ApiPaths variables to read

--- src/main/java/ru/d2k/parkle/controller/ApiPaths.java
+++ src/main/java/ru/d2k/parkle/controller/ApiPaths.java
@@ -1,12 +1,9 @@
 package ru.d2k.parkle.controller;
 
 public interface ApiPaths {
-    String API = "/api/";
-
-    String ROLE_API = API + "roles/";
-    String USER_API = API + "users/";
-    String WEBSITE_API = API + "websites/";
-    String AUTH_API = API + "auth/";
-    String AUTH_LOGIN_API = AUTH_API + "login/";
-    String AUTH_REGISTRATION_API = AUTH_API + "registration/";
+    String ROLE_API = "/api/roles/";
+    String USER_API = "/api/users/";
+    String WEBSITE_API = "/api/websites/";
+    String AUTH_LOGIN_API = "/api/auth/login/";
+    String AUTH_REGISTRATION_API = "/api/auth/registration/";
 }
