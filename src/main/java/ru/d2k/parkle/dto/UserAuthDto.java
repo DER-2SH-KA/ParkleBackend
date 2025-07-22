137c14bbef8190c3e495499e8daa3ee6ab8509d4
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-22 20:10:52 +0700

    feat: Add UserAuthDto

---
+++ src/main/java/ru/d2k/parkle/dto/UserAuthDto.java
@@ -0,0 +1,26 @@
+package ru.d2k.parkle.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(exclude = "password")
+@EqualsAndHashCode(exclude = "password")
+public class UserAuthDto {
+    @NotBlank(message = "User's Auth login must not be null or blank")
+    @Size(max = 100, message = "User's Auth login length must be lower than 100 symbols")
+    private String login;
+
+    @NotBlank(message = "User's Auth password must not be null or blank")
+    @Size(
+            min = 8,
+            max = 72,
+            message = "User's Auth password lengths must be between 8 and 72 symbols"
+    )
+    private String password;
+}
