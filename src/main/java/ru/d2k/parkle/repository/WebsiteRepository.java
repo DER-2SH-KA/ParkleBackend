cac23bcbe1aec2f64a6fe624c087cf4a69f047ec
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-22 19:02:02 +0700

    feat: Add findByUserIdOrderByTitleAsc method in WebsiteRepository

--- src/main/java/ru/d2k/parkle/repository/WebsiteRepository.java
+++ src/main/java/ru/d2k/parkle/repository/WebsiteRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.d2k.parkle.entity.Website;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface WebsiteRepository extends JpaRepository<Website, UUID> {
+    List<Website> findByUserIdOrderByTitleAsc(UUID userId);
 }
 
