b868a5ebb4d6524514984fd0e3c566e070e601ee
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-24 21:20:32 +0700

    fix: Edit ApiPaths values

--- src/main/java/ru/d2k/parkle/controller/ApiPaths.java
+++ src/main/java/ru/d2k/parkle/controller/ApiPaths.java
@@ -1,8 +1,8 @@
 package ru.d2k.parkle.controller;
 
 public interface ApiPaths {
-    String ROLE_API = "/api/roles/";
-    String USER_API = "/api/users/";
-    String WEBSITE_API = "/api/websites/";
-    String AUTH_API = "/api/auth/";
+    String ROLE_API = "/api/roles";
+    String USER_API = "/api/users";
+    String WEBSITE_API = "/api/websites";
+    String AUTH_API = "/api/auth";
 }
