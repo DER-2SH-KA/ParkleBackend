0a5dfca56a399b686eacacad34cf4d3a5f3ba5c8
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-26 15:43:28 +0700

    refactor: Replace logs in UserRestController to AOP logs

--- src/main/java/ru/d2k/parkle/controller/rest/UserRestController.java
+++ src/main/java/ru/d2k/parkle/controller/rest/UserRestController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.d2k.parkle.aspect.annotation.LoggableGetMapping;
 import ru.d2k.parkle.controller.ApiPaths;
 import ru.d2k.parkle.dto.UserCreateDto;
 import ru.d2k.parkle.dto.UserResponseDto;
@@ -13,7 +14,6 @@ import ru.d2k.parkle.service.rest.UserService;
 import java.util.UUID;
 import java.util.Set;
 
-@Slf4j
 @RestController
 @RequestMapping(ApiPaths.USER_API)
 @RequiredArgsConstructor
@@ -25,12 +25,10 @@ public class UserRestController {
      * @return List of {@link UserResponseDto}.
      * **/
     @GetMapping
+    @LoggableGetMapping
     public ResponseEntity<Set<UserResponseDto>> findUsers() {
-        log.info("Given GET request to return all users");
-
         Set<UserResponseDto> userResponseDtos = userService.findUsers();
 
-        log.info("GET request was served");
         return ResponseEntity.ok( userResponseDtos );
     }
 
@@ -40,12 +38,10 @@ public class UserRestController {
      * @return {@link UserResponseDto} object.
      * **/
     @GetMapping("/{id}")
+    @LoggableGetMapping
     public ResponseEntity<UserResponseDto> findUserById(@PathVariable UUID id) {
-        log.info("Given GET request to return user by ID: {}", id);
-
         UserResponseDto responseDto = userService.findUserById(id);
 
-        log.info("GET request was served");
         return ResponseEntity.ok( responseDto );
     }
 }
