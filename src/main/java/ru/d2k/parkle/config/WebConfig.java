e352d4f6bad2045b038426a238423691de37cfc4
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-07-30 23:07:42 +0700

    refactor: delete unused code

--- src/main/java/ru/d2k/parkle/config/WebConfig.java
+++ src/main/java/ru/d2k/parkle/config/WebConfig.java
@@ -2,11 +2,9 @@ package ru.d2k.parkle.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
