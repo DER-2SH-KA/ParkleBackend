2099ffc4c4e3900633f06d99bc3d46a93c24c224
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-08-02 22:11:07 +0700

    feat: add bcrypt method and values in application files (+ .env)

--- src/main/java/ru/d2k/parkle/config/SecurityConfig.java
+++ src/main/java/ru/d2k/parkle/config/SecurityConfig.java
@@ -1,15 +1,24 @@
 package ru.d2k.parkle.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(@Value("${spring.security.bcrypt.strength}") int strength) {
+        return new BCryptPasswordEncoder( strength );
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> {
