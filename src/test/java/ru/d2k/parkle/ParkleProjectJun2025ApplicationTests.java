b6bbc376c65a16b1c74a045f47eb7c5df7b716f0
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-08-04 22:43:01 +0700

    fix: fix crash of tests by .\mvnw tests

--- src/test/java/ru/d2k/parkle/ParkleProjectJun2025ApplicationTests.java
+++ src/test/java/ru/d2k/parkle/ParkleProjectJun2025ApplicationTests.java
@@ -2,8 +2,10 @@ package ru.d2k.parkle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ParkleProjectJun2025ApplicationTests {
 
 	@Test
