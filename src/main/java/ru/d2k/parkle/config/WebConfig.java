d727943f37ebb37d1fc32e4a341efb1a30f94f20
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-08-04 22:45:35 +0700

    feat: add IPv4 address and domain parkle-der2shka as allowed origins

--- src/main/java/ru/d2k/parkle/config/WebConfig.java
+++ src/main/java/ru/d2k/parkle/config/WebConfig.java
@@ -10,8 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://81.31.247.85:8080",
+                        "http://parkle-der2shka.ru:8080"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+
     }
 
 }
