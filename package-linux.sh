5f3e2f912a0f9ec69cadbfbef5bb58bf621f8fa5
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-08-06 19:03:19 +0700

    feat: add linux execution files

---
+++ package-linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Start Package script for parkle"
+./mvnw package && read -s -n 1
---
+++ start-linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./mvnw -Dspring-boot.run.profiles=dev spring-boot:run && read -s -n 1
