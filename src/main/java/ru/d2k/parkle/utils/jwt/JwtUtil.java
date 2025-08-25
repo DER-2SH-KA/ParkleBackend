ac60256743c75af717deee1609dfbbc2038251bc
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-08-26 01:18:58 +0700

    refactor: delete unused imports

--- src/main/java/ru/d2k/parkle/utils/jwt/JwtUtil.java
+++ src/main/java/ru/d2k/parkle/utils/jwt/JwtUtil.java
@@ -2,7 +2,6 @@ package ru.d2k.parkle.utils.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
-import java.security.PrivateKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
