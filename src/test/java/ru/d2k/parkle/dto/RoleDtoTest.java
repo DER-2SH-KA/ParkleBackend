bacb7871bce6d7d48fcc6b069cf6427380ebcc97
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-09-10 22:25:43 +0700

    test: add tests in RoleDtoTest

--- src/test/java/ru/d2k/parkle/dto/RoleDtoTest.java
+++ src/test/java/ru/d2k/parkle/dto/RoleDtoTest.java
@@ -1,56 +1,26 @@
 package ru.d2k.parkle.dto;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.d2k.parkle.utils.generator.Uuid7Generator;
 
 import java.util.UUID;
-
-/* TODO: Измени меня! Вместо кучи маленьких несколько важных тестов:
-*   - Тестирование equals с похожими объектами;
-*   - Параметризированное тестирование equals с разными полями объектов;
-*   - Тестирование с Null объектами и полями в объектах;
-*   - Тестирование через Jakarta Validation.
-* */
+import java.util.stream.Stream;
+import java.util.Set;
 
 public class RoleDtoTest {
-    @DisplayName("equals - return true with same object")
-    @Test
-    public void shouldBeTrueWhenEqualsWithSameObject() {
-        RoleDto roleDto = new RoleDto(Uuid7Generator.generateNewUUID(), "Role", 0);
-
-        Assertions.assertTrue(roleDto.equals(roleDto));
-    }
-
-    @DisplayName("equals - return false with null")
-    @Test
-    public void shouldBeFalseWhenEqualsWithNull() {
-        RoleDto roleDto = new RoleDto(Uuid7Generator.generateNewUUID(), "Role", 0);
-
-        Assertions.assertFalse(roleDto.equals(null));
-    }
-
-    @DisplayName("equals - return false with another type of class")
-    @Test
-    public void shouldBeFalseWhenEqualsWithAnotherClassType() {
-        RoleDto roleDto = new RoleDto(Uuid7Generator.generateNewUUID(), "Role", 0);
-
-        Assertions.assertNotEquals(roleDto, new Object());
-    }
-
-    @DisplayName("equals - return false with object which has null fields")
-    @Test
-    public void shouldBeFalseWhenEqualsWithObjectWhichFieldsAreNull() {
-        RoleDto roleDto = new RoleDto(Uuid7Generator.generateNewUUID(), "Role", 0);
-
-        Assertions.assertNotEquals(roleDto, new RoleDto(null, null, null));
-    }
+    private static final UUID uuid = Uuid7Generator.generateNewUUID();
+    private static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
     @DisplayName("hashCode/equals - return true with similar objects")
     @Test
     public void shouldBeTrueWhenEqualsAndHashCodeWithSimilarObjects() {
-        UUID uuid = Uuid7Generator.generateNewUUID();
         RoleDto roleDto1 = new RoleDto(uuid, "Role", 0);
         RoleDto roleDto2 = new RoleDto(uuid, "Role", 0);
 
@@ -58,30 +28,65 @@ public class RoleDtoTest {
         Assertions.assertEquals(roleDto1, roleDto2);
     }
 
-    @DisplayName("equals - return false with objects which has different fields")
+    @DisplayName("equals - return false with null")
     @Test
-    public void shouldBeFalseWhenEqualsWithObjectWhichHasDiffFields() {
-        RoleDto roleDto1 = new RoleDto(Uuid7Generator.generateNewUUID(), "Role1", 1);
-        RoleDto roleDto2 = new RoleDto(Uuid7Generator.generateNewUUID(), "Role2", 2);
+    public void shouldBeFalseWhenEqualsWithNull() {
+        RoleDto roleDto = new RoleDto(uuid, "Role", 0);
+
+        Assertions.assertFalse(roleDto.equals(null));
+    }
+
+    @DisplayName("equals - return false with object which has null fields")
+    @Test
+    public void shouldBeFalseWhenEqualsWithObjectWhichFieldsAreNull() {
+        RoleDto roleDto = new RoleDto(uuid, "Role", 0);
+
+        Assertions.assertNotEquals(roleDto, new RoleDto(null, null, null));
+    }
+
+    @DisplayName("equals - return false with objects which has different fields")
+    @ParameterizedTest
+    @MethodSource
+    public void shouldBeFalseWhenEqualsWithObjectWhichHasDiffFields(RoleDto roleDto2) {
+        RoleDto roleDto1 = new RoleDto(uuid, "Role1", 1);
 
         Assertions.assertNotEquals(roleDto1, roleDto2);
     }
 
-    @DisplayName("hashCode - return true with object which has all fields are null")
-    @Test
-    public void shouldBeEqualsWhenHashCodeWithObjectsWhichHasNullFields() {
-        RoleDto roleDto1 = new RoleDto(null, null, null);
-        RoleDto roleDto2 = new RoleDto(null, null, null);
+    @DisplayName("validate - return false with objects which has wrong field's values")
+    @ParameterizedTest
+    @MethodSource
+    public void shouldBeFalseWhenValidationObjectHasWrongFieldsValues(RoleDto roleDto) {
+        Set<ConstraintViolation<RoleDto>> violations = validator.validate(roleDto);
 
-        Assertions.assertEquals(roleDto1.hashCode(), roleDto2.hashCode());
+        Assertions.assertFalse(violations.isEmpty());
     }
 
-    @DisplayName("hashCode - return true with object which has part of fields are null")
+    @DisplayName("validate - return true with objects which has correct field's values")
     @Test
-    public void shouldBeEqualsWhenHashCodeWithObjectsWhichHasPartOfNullFields() {
-        RoleDto roleDto1 = new RoleDto(null, "Role", null);
-        RoleDto roleDto2 = new RoleDto(null, "Role", null);
+    public void shouldBeTrueWhenValidationObjectHasCorrectFieldsValues() {
+        RoleDto roleDto = new RoleDto(uuid, "Developer", 1);
+        Set<ConstraintViolation<RoleDto>> violations = validator.validate(roleDto);
 
-        Assertions.assertEquals(roleDto1.hashCode(), roleDto2.hashCode());
+        Assertions.assertTrue(violations.isEmpty());
+    }
+
+    private static Stream<RoleDto> shouldBeFalseWhenEqualsWithObjectWhichHasDiffFields() {
+        return Stream.of(
+                new RoleDto(Uuid7Generator.generateNewUUID(), "Role1", 1),
+                new RoleDto(uuid, "Role2", 1),
+                new RoleDto(uuid, "Role1", 2)
+        );
+    }
+
+    private static Stream<RoleDto> shouldBeFalseWhenValidationObjectHasWrongFieldsValues() {
+        return Stream.of(
+                new RoleDto(uuid, null, 1),
+                new RoleDto(uuid, "", 1),
+                new RoleDto(uuid, "12345123451234512345123451234512345", 1),
+                new RoleDto(uuid, "Role", null),
+                new RoleDto(uuid, "Role", -1),
+                new RoleDto(uuid, "Role", 0)
+        );
     }
 }
