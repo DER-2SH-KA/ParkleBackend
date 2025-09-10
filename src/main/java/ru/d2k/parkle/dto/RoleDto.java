e39e87c4c746097cfbdbb5f37cdc75297b74de50
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-09-10 22:00:38 +0700

    refactor: add @Size(max) for name in RoleDto

--- src/main/java/ru/d2k/parkle/dto/RoleDto.java
+++ src/main/java/ru/d2k/parkle/dto/RoleDto.java
@@ -3,6 +3,7 @@ package ru.d2k.parkle.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import ru.d2k.parkle.entity.Role;
 
@@ -16,6 +17,10 @@ public class RoleDto {
     private UUID id;
 
     @NotBlank(message = "RoleDto name is null or blank.")
+    @Size(
+            max = 32,
+            message = "RoleDto name's length grater than 32 symbol"
+    )
     private String name;
 
     @NotNull(message = "RoleDto priority is null")
