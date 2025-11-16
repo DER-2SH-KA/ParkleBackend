419f19e248efabd9469e3995a04f01bea35c65bb
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-11-16 23:51:08 +0700

    feat: add WebsiteIsExtremismSourceException

---
+++ src/main/java/ru/d2k/parkle/exception/WebsiteIsExtremismSourceException.java
@@ -0,0 +1,6 @@
+package ru.d2k.parkle.exception;
+
+public class WebsiteIsExtremismSourceException extends RuntimeException {
+    public WebsiteIsExtremismSourceException() { super(); }
+    public WebsiteIsExtremismSourceException(String message) { super(message); }
+}
