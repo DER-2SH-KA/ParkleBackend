94a9b5768453313e3f304513e02f7ddc8cde21ab
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-11-22 01:45:40 +0700

    refactor: add ping permits for all requests

--- src/main/java/ru/d2k/parkle/config/SecurityConfig.java
+++ src/main/java/ru/d2k/parkle/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
                                         "/*.js",
                                         "/*.css",
                                         "/*.ico",
-                                        "/assets/**"
+                                        "/assets/**",
+                                        "/api/auth/ping"
                                 ).permitAll()
                                 .requestMatchers(
                                         "/api/roles",
