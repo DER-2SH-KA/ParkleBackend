c1247cbeec6cda987a2b91cd17a9c314ef0ef95e
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-12-03 00:24:59 +0700

    test: add tests in UserRepositoryTest

--- src/test/java/ru/d2k/parkle/repository/UserRepositoryTest.java
+++ src/test/java/ru/d2k/parkle/repository/UserRepositoryTest.java
@@ -1,4 +1,86 @@
 package ru.d2k.parkle.repository;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.d2k.parkle.entity.Role;
+import ru.d2k.parkle.entity.User;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByLogin_shouldFindNewUserByLogin() {
+        Role newRole = Role.create("Test", 10);
+        User newUser = User.create(newRole, "Login", "Mail@mail.ru", "Password");
+
+        testEntityManager.persistAndFlush(newRole);
+        testEntityManager.persistAndFlush(newUser);
+
+        Optional<User> savedUser = userRepository.findByLogin("Login");
+
+        Assertions
+                .assertThat(savedUser)
+                .isPresent();
+        Assertions
+                .assertThat(savedUser.get().getLogin())
+                .isEqualTo("Login");
+    }
+
+    @Test
+    void existsById_shouldSaveAndExistsByIdNewUser() {
+        Role newRole = Role.create("Test", 10);
+        User newUser = User.create(newRole, "Login", "Mail@mail.ru", "Password");
+
+        testEntityManager.persistAndFlush(newRole);
+        testEntityManager.persistAndFlush(newUser);
+
+        boolean isUserExists = userRepository.existsById(newUser.getId());
+
+        Assertions
+                .assertThat(isUserExists)
+                .isTrue();
+    }
+
+    @Test
+    void existsByLogin_shouldSaveAndExistsByLoginNewUser() {
+        Role newRole = Role.create("Test", 10);
+        User newUser = User.create(newRole, "Login", "Mail@mail.ru", "Password");
+
+        testEntityManager.persistAndFlush(newRole);
+        testEntityManager.persistAndFlush(newUser);
+
+        boolean isUserExists = userRepository.existsByLogin("Login");
+
+        Assertions
+                .assertThat(isUserExists)
+                .isTrue();
+    }
+
+    @Test
+    void deleteByLogin_shouldSaveAndExistsByLoginNewUser() {
+        Role newRole = Role.create("Test", 10);
+        User newUser = User.create(newRole, "Login", "Mail@mail.ru", "Password");
+
+        testEntityManager.persistAndFlush(newRole);
+        testEntityManager.persistAndFlush(newUser);
+
+        userRepository.deleteByLogin("Login");
+
+        Assertions
+                .assertThat(userRepository.existsByLogin("Login"))
+                .isFalse();
+    }
 }
