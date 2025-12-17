67c7985dc6c32e9ad63b9d9f29cfd2da77ec12e7
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-12-17 14:00:49 +0700

    db: change User DTOes login length from 100 to 50

--- src/main/java/ru/d2k/parkle/dto/UserAuthDto.java
+++ src/main/java/ru/d2k/parkle/dto/UserAuthDto.java
@@ -1,6 +1,7 @@
 package ru.d2k.parkle.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -11,15 +12,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserAuthDto {
-    @NotBlank(message = "User's Auth login must not be null or blank")
-    @Size(max = 100, message = "User's Auth login length must be lower than 100 symbols")
+    @NotBlank(message = "UserAuthDto login must not be null or blank")
+    @Size(
+            min = 3,
+            max = 50,
+            message = "UserAuthDto login length must be between 3 and 50 symbols"
+    )
+    @Pattern(regexp = "^[a-zA-Z0-9]{3,50}$", message = "UserAuthDto login isn't allowed by regexp pattern")
     private String login;
 
-    @NotBlank(message = "User's Auth password must not be null or blank")
+    @NotBlank(message = "UserAuthDto password must not be null or blank")
     @Size(
             min = 8,
             max = 72,
-            message = "User's Auth password lengths must be between 8 and 72 symbols"
+            message = "UserAuthDto password lengths must be between 8 and 72 symbols"
     )
     private String password;
 }
--- src/main/java/ru/d2k/parkle/dto/UserCreateDto.java
+++ src/main/java/ru/d2k/parkle/dto/UserCreateDto.java
@@ -11,29 +11,31 @@ import lombok.*;
 @AllArgsConstructor
 public class UserCreateDto {
 
-    @NotBlank(message = "UserCreate must have any role (by name)")
+    @NotBlank(message = "UserCreateDto must have any role (by name)")
     private String roleName;
 
-    @NotBlank(message = "UserCreate's login can't be null or blank")
+    @NotBlank(message = "UserCreateDto login can't be null or blank")
     @Size(
-            max = 100,
-            message = "UserCreate's login size must be lower then 100 symbols"
+            min = 3,
+            max = 50,
+            message = "UserCreateDto login size must be between 3 and 50 symbols"
     )
+    @Pattern(regexp = "^[a-zA-Z0-9]{3,50}$", message = "UserCreateDto login isn't allowed by regexp pattern")
     private String login;
 
-    @NotBlank(message = "UserCreate's email can't be null or blank")
+    @NotBlank(message = "UserCreateDto email can't be null or blank")
     @Size(
             max = 320,
-            message = "UserCreate's email size must be lower then 320 symbols"
+            message = "UserCreateDto email size must be lower then 320 symbols"
     )
-    @Email(message = "UserCreate's email is not valid")
+    @Email(message = "UserCreateDto email is not valid")
     private String email;
 
-    @NotBlank(message = "UserCreate's password can't be null or blank")
+    @NotBlank(message = "UserCreateDto password can't be null or blank")
     @Size(
             min = 8,
             max = 72,
-            message = "UserCreate's password length must be between 8 and 72"
+            message = "UserCreateDto password length must be between 8 and 72"
     )
     private String password;
 
