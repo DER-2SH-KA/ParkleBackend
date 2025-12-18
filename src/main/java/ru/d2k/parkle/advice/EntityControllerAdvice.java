bef13b28a5fa7d96ebb6f944a1e4bbd1fecaf621
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-12-18 23:52:57 +0700

    refactor: add @Slf4j and replace @ControllerAdvice to @RestControllerAdvice in controller advices

--- src/main/java/ru/d2k/parkle/advice/EntityControllerAdvice.java
+++ src/main/java/ru/d2k/parkle/advice/EntityControllerAdvice.java
@@ -1,27 +1,50 @@
 package ru.d2k.parkle.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.d2k.parkle.exception.*;
 
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice
 public class EntityControllerAdvice {
 
-    @ExceptionHandler({RoleNotFoundException.class, UserNotFoundException.class, WebsiteNotFoundException.class})
-    public ResponseEntity<?> handleNotFoundException(RuntimeException ex) {
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<?> handleRoleNotFoundException(RuntimeException ex) {
+        log.error("Role entity not found! Message: {}", ex.getMessage());
+
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(RuntimeException ex) {
+        log.error("User entity not found! Message: {}", ex.getMessage());
+
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(WebsiteNotFoundException.class)
+    public ResponseEntity<?> handleWebsiteNotFoundException(RuntimeException ex) {
+        log.error("Website entity not found! Message: {}", ex.getMessage());
+
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     // TODO: избавиться ото всех try-catch, чтобы все ошибки всплывали вплоть до контроллеров!
-    @ExceptionHandler(UserWrongPasswordException.class)
+    @ExceptionHandler({UserWrongPasswordException.class, UsernameNotFoundException.class})
     public ResponseEntity<?> handleWrongPasswordException(RuntimeException ex) {
+        log.error("User not found with this login and password. Message: {}", ex.getMessage());
+
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(WebsiteIsExtremismSourceException.class)
     public ResponseEntity<?> handleExtremismSourceException(RuntimeException ex) {
+        log.error("Website with this URL contains in extremism list. Message: {}", ex.getMessage());
+
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
--- src/main/java/ru/d2k/parkle/advice/JwtControllerAdvice.java
+++ src/main/java/ru/d2k/parkle/advice/JwtControllerAdvice.java
@@ -1,17 +1,21 @@
 package ru.d2k.parkle.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.d2k.parkle.exception.JwtNotExistInRequestException;
 import ru.d2k.parkle.exception.JwtNotIncludeUserLoginException;
 
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice
 public class JwtControllerAdvice {
 
     @ExceptionHandler({JwtNotExistInRequestException.class, JwtNotIncludeUserLoginException.class})
     public ResponseEntity<?> handleJwtException(RuntimeException ex) {
+        log.error("JWT error. Message: {}", ex.getMessage());
+
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
