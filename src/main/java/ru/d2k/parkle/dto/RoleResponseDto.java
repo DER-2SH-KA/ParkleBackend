f0864c5caa6c166c8eca1c9d41f148462cf332dd
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-05 00:10:09 +0700

    refactor: make RoleResponseDto to record class

--- src/main/java/ru/d2k/parkle/dto/RoleResponseDto.java
+++ src/main/java/ru/d2k/parkle/dto/RoleResponseDto.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class RoleResponseDto {
-    private String name;
-    private Integer priority;
-}
+public record RoleResponseDto(
+        String name,
+        Integer priority
+) { }
