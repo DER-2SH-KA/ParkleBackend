c6f8ff9a00d67fac8c44892f689db2ec76e2c4ff
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-05 00:19:50 +0700

    refactor: make UserResponseDto to record class

--- src/main/java/ru/d2k/parkle/dto/UserResponseDto.java
+++ src/main/java/ru/d2k/parkle/dto/UserResponseDto.java
@@ -1,16 +1,10 @@
 package ru.d2k.parkle.dto;
 
-import lombok.*;
-
 import java.util.UUID;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserResponseDto {
-    private UUID id;
-    private RoleResponseDto role;
-    private String login;
-    private String email;
-
-}
+public record UserResponseDto(
+        UUID id,
+        RoleResponseDto role,
+        String login,
+        String email
+) { }
