d3adadd98ce2f3efbb413c86e9cfbfc00135b44d
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-05 00:19:31 +0700

    refactor: delete unused imports in dto record classes

--- src/main/java/ru/d2k/parkle/dto/RoleResponseDto.java
+++ src/main/java/ru/d2k/parkle/dto/RoleResponseDto.java
@@ -1,9 +1,5 @@
 package ru.d2k.parkle.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 public record RoleResponseDto(
         String name,
         Integer priority
