b5b9b1804e48e192feb00d8731ee36d4b6e23c82
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-05 00:20:17 +0700

    refactor: make WebsiteUpdateDto to record class

--- src/main/java/ru/d2k/parkle/dto/WebsiteUpdateDto.java
+++ src/main/java/ru/d2k/parkle/dto/WebsiteUpdateDto.java
@@ -1,36 +1,30 @@
 package ru.d2k.parkle.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class WebsiteUpdateDto {
+public record WebsiteUpdateDto(
+        @NotNull(message = "WebsiteUpdateDto userId can't be null")
+        UUID userId,
 
-    @Pattern(
-            regexp = "^#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$",
-            message = "WebsiteUpdateDto hexColor must be type as #fff or #FFFFFF"
-    )
-    private String hexColor;
+        @NotBlank(message = "WebsiteUpdateDto hexColorValue cannot be null or blank")
+        @Pattern(
+                regexp = "^#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$",
+                message = "WebsiteUpdateDto hexColor must be type as #fff or #FFFFFF"
+        )
+        String hexColor,
 
-    private UUID userId;
+        @NotBlank(message = "WebsiteUpdateDto title can't be null or blank")
+        @Size(max = 40, message = "WebsiteUpdateDto title length must be lower than or equal 40 symbols")
+        String title,
 
-    @Size(
-            max = 40,
-            message = "WebsiteUpdateDto title length must be lower than or equal 40 symbols"
-    )
-    private String title;
+        @Size(max = 255, message = "WebsiteUpdateDto title length must be lower than 255 symbols")
+        String description,
 
-    @Size(max = 255, message = "WebsiteUpdateDto description length must be lower than 255 symbols")
-    private String description;
-
-    private String url;
-
-}
+        @NotBlank(message = "WebsiteUpdateDto URL can't be null or blank")
+        String url
+) { }
