a5b4f0e4d71830e55e83a922fa3a4df326ee9292
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-05 00:20:10 +0700

    refactor: make WebsiteResponseDto to record class

--- src/main/java/ru/d2k/parkle/dto/WebsiteResponseDto.java
+++ src/main/java/ru/d2k/parkle/dto/WebsiteResponseDto.java
@@ -1,19 +1,12 @@
 package ru.d2k.parkle.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.UUID;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class WebsiteResponseDto {
-    private UUID id;
-    private UUID userId;
-    private String hexColor;
-    private String title;
-    private String description;
-    private String url;
-}
+public record WebsiteResponseDto(
+        UUID id,
+        UUID userId,
+        String hexColor,
+        String title,
+        String description,
+        String url
+) { }
