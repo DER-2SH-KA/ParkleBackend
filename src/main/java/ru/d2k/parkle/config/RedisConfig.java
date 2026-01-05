accca53a9f8b73767c5cda6e0c551304abbda019
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-05 20:23:41 +0700

    feat: add RedisTemplate<String, Role> redisRoleTemplate Bean in RedisConfig

--- src/main/java/ru/d2k/parkle/config/RedisConfig.java
+++ src/main/java/ru/d2k/parkle/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import ru.d2k.parkle.entity.Role;
 
 @Configuration
 public class RedisConfig {
@@ -27,6 +28,23 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Role> redisRoleTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Role> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         StringRedisSerializer stringSerializer = new StringRedisSerializer();
