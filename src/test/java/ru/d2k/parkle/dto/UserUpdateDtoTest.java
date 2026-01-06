fa0b356e693bbf00d2a275dee1cf19f3c123a5db
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-07 02:23:27 +0700

    refactor: fix UserUpdateDtoTest positive validation tests and add in parameterized tests Arguments

--- src/test/java/ru/d2k/parkle/dto/UserUpdateDtoTest.java
+++ src/test/java/ru/d2k/parkle/dto/UserUpdateDtoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.d2k.parkle.utils.generator.Uuid7Generator;
 
@@ -66,7 +67,7 @@ public class UserUpdateDtoTest {
     public void shouldBeTrueWhenValidationObjectsHasCorrectFieldsValues(UserUpdateDto dto) {
         Set<ConstraintViolation<UserUpdateDto>> violations = validator.validate(dto);
 
-        Assertions.assertTrue(violations.isEmpty());
+        Assertions.assertTrue(violations.isEmpty(), "Violation founds: " + violations);
     }
 
     @DisplayName("validate - return false with objects which has wrong field's values")
@@ -87,36 +88,36 @@ public class UserUpdateDtoTest {
         );
     }
 
-    private static Stream<UserUpdateDto> shouldBeTrueWhenValidationObjectsHasCorrectFieldsValues() {
-        final char[] overMaxRoleNameChars = new char[33];
-        final char[] lowerMinLoginChars = new char[2];
-        final char[] overMaxLoginChars = new char[51];
-        final char[] overMaxEmailChars = new char[321];
-        final char[] overMaxPasswordChars = new char[73];
+    private static Stream<Arguments> shouldBeTrueWhenValidationObjectsHasCorrectFieldsValues() {
+        final char[] maxRoleNameChars = new char[32];
+        final char[] lowMinLoginChars = new char[3];
+        final char[] maxLoginChars = new char[50];
+        final char[] maxEmailChars = new char[312];
+        final char[] maxPasswordChars = new char[72];
 
-        Arrays.fill(overMaxRoleNameChars, 'r');
-        Arrays.fill(lowerMinLoginChars, 'l');
-        Arrays.fill(overMaxLoginChars, 'l');
-        Arrays.fill(overMaxEmailChars, 'e');
-        Arrays.fill(overMaxPasswordChars, 'p');
+        Arrays.fill(maxRoleNameChars, 'r');
+        Arrays.fill(lowMinLoginChars, 'l');
+        Arrays.fill(maxLoginChars, 'l');
+        Arrays.fill(maxEmailChars, 'e');
+        Arrays.fill(maxPasswordChars, 'p');
 
-        final String overMaxRoleName = new String(overMaxRoleNameChars);
-        final String lowerMinLogin = new String(lowerMinLoginChars);
-        final String overMaxLogin = new String(overMaxLoginChars);
-        final String overMaxEmail = new String(overMaxEmailChars);
-        final String overMaxPassword = new String(overMaxPasswordChars);
+        final String maxRoleName = new String(maxRoleNameChars);
+        final String lowMinLogin = new String(lowMinLoginChars);
+        final String maxLogin = new String(maxLoginChars);
+        final String maxEmail = new String(maxEmailChars) + "@mail.ru";
+        final String maxPassword = new String(maxPasswordChars);
 
         return Stream.of(
-                new UserUpdateDto(ID, overMaxRoleName, LOGIN, EMAIL, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, lowerMinLogin, EMAIL, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, overMaxLogin, EMAIL, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, LOGIN, overMaxEmail, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, "1234567"),
-                new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, overMaxPassword)
+                Arguments.of(new UserUpdateDto(ID, maxRoleName, LOGIN, EMAIL, PASSWORD), "Valid maxRoleName"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, lowMinLogin, EMAIL, PASSWORD), "Valid lowMinLogin"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, maxLogin, EMAIL, PASSWORD), "Valid maxLogin"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, LOGIN, maxEmail, PASSWORD), "Valid maxEmail"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, "12345678"), "Valid lowMinPassword"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, maxPassword), "Valid maxPassword")
         );
     }
 
-    private static Stream<UserUpdateDto> shouldBeFalseWhenValidationObjectsHasWrongFieldsValues() {
+    private static Stream<Arguments> shouldBeFalseWhenValidationObjectsHasWrongFieldsValues() {
         final char[] overMaxRoleNameChars = new char[33];
         final char[] lowerMinLoginChars = new char[2];
         final char[] overMaxLoginChars = new char[51];
@@ -136,12 +137,12 @@ public class UserUpdateDtoTest {
         final String overMaxPassword = new String(overMaxPasswordChars);
 
         return Stream.of(
-                new UserUpdateDto(ID, overMaxRoleName, LOGIN, EMAIL, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, lowerMinLogin, EMAIL, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, overMaxLogin, EMAIL, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, LOGIN, overMaxEmail, PASSWORD),
-                new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, "1234567"),
-                new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, overMaxPassword)
+                Arguments.of(new UserUpdateDto(ID, overMaxRoleName, LOGIN, EMAIL, PASSWORD), "Valid overMaxRoleName"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, lowerMinLogin, EMAIL, PASSWORD), "Valid lowerMinLogin"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, overMaxLogin, EMAIL, PASSWORD), "Valid overMaxLogin"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, LOGIN, overMaxEmail, PASSWORD), "Valid overMaxEmail"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, "1234567"), "Valid lowerMinRoleName"),
+                Arguments.of(new UserUpdateDto(ID, ROLE_NAME, LOGIN, EMAIL, overMaxPassword), "Valid overMaxPassword")
         );
     }
 }
