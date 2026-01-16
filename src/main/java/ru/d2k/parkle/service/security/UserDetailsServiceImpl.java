1a2756e3154eb858070da2a4066c372719062291
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-17 02:11:15 +0700

    refactor: replace Entity to Cache object in UserDetailsServiceImpl

--- src/main/java/ru/d2k/parkle/service/security/UserDetailsServiceImpl.java
+++ src/main/java/ru/d2k/parkle/service/security/UserDetailsServiceImpl.java
@@ -5,8 +5,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.d2k.parkle.dao.UserDao;
 import ru.d2k.parkle.entity.Role;
 import ru.d2k.parkle.entity.User;
+import ru.d2k.parkle.entity.cache.UserCache;
 import ru.d2k.parkle.model.CustomUserDetails;
 import ru.d2k.parkle.repository.RoleRepository;
 import ru.d2k.parkle.repository.UserRepository;
@@ -16,11 +18,11 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final UserRepository userRepository;
+    private final UserDao userDao;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = userRepository.findByLogin(username);
+        Optional<UserCache> user = userDao.getByLogin(username);
 
         if (user.isPresent()) {
             return new CustomUserDetails(user.get());
