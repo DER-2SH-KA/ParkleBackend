9cc3b1292a73e54172b83c5044a847d5d99e9b15
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-17 02:10:04 +0700

    feat: add toResponseDto and toCache in UserMapper

--- src/main/java/ru/d2k/parkle/utils/mapper/UserMapper.java
+++ src/main/java/ru/d2k/parkle/utils/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import ru.d2k.parkle.dto.UserResponseDto;
 import ru.d2k.parkle.dto.UserUpdateDto;
 import ru.d2k.parkle.entity.Role;
 import ru.d2k.parkle.entity.User;
+import ru.d2k.parkle.entity.cache.UserCache;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -20,4 +21,17 @@ public interface UserMapper {
     @Mapping(target = "websites", ignore = true)
     User updateByDto(@MappingTarget User entity, UserUpdateDto dto, Role role);
 
+    /**
+     * CACHE => DTO
+     * */
+    UserResponseDto toResponseDto(UserCache userCache);
+
+    /**
+     * ENTITY => CACHE
+     * */
+    @Mapping(target = "roleId", source = "role.id")
+    @Mapping(target = "roleName", source = "role.name")
+    @Mapping(target = "rolePriority", source = "role.priority")
+    @Mapping(target = "hashedPassword", source = "password")
+    UserCache toCache(User entity);
 }
