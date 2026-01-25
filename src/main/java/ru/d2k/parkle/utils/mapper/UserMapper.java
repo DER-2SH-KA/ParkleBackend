b9c8ae34237254e0850774cf1a6373c68090605d
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-25 17:42:22 +0700

    feat: add fromWebsitesToUUID in UserMapper

--- src/main/java/ru/d2k/parkle/utils/mapper/UserMapper.java
+++ src/main/java/ru/d2k/parkle/utils/mapper/UserMapper.java
@@ -5,7 +5,11 @@ import ru.d2k.parkle.dto.UserResponseDto;
 import ru.d2k.parkle.dto.UserUpdateDto;
 import ru.d2k.parkle.entity.Role;
 import ru.d2k.parkle.entity.User;
+import ru.d2k.parkle.entity.Website;
 import ru.d2k.parkle.entity.cache.UserCache;
+
+import java.util.List;
+import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -33,5 +37,14 @@ public interface UserMapper {
     @Mapping(target = "roleName", source = "role.name")
     @Mapping(target = "rolePriority", source = "role.priority")
     @Mapping(target = "hashedPassword", source = "password")
+    @Mapping(target = "isBlocked", source = "isBlocked")
+    @Mapping(target = "websiteIds", source = "websites", qualifiedByName = "fromWebsitesToUUID")
     UserCache toCache(User entity);
+
+    @Named("fromWebsitesToUUID")
+    default List<UUID> fromWebsitesToUUID(List<Website> websites) {
+        return websites.stream()
+                .map(Website::getId)
+                .toList();
+    }
 }
