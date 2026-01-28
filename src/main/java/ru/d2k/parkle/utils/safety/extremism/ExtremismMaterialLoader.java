ad5a4b12e636074403ccf1b7a89e616027d6ebb8
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-01-29 04:30:21 +0700

    refactor: do ExtremismMaterialLoader to interface

--- src/main/java/ru/d2k/parkle/utils/safety/extremism/ExtremismMaterialLoader.java
+++ src/main/java/ru/d2k/parkle/utils/safety/extremism/ExtremismMaterialLoader.java
@@ -2,6 +2,6 @@ package ru.d2k.parkle.utils.safety.extremism;
 
 import java.util.Set;
 
-public abstract class ExtremismMaterialLoader {
-    public abstract Set<String> getExtremismLinks();
+public interface ExtremismMaterialLoader {
+    Set<String> getExtremismLinks();
 }
