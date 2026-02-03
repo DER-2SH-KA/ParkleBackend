71c5c2d24cae71ba647571f26409e851be506927
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-02-03 20:58:51 +0700

    feat: add RoleRedisCache and RolePostgresDatabase classes

---
+++ src/main/java/ru/d2k/parkle/dao/cache/role/RoleRedisCache.java
@@ -0,0 +1,30 @@
+package ru.d2k.parkle.dao.cache.role;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import ru.d2k.parkle.entity.cache.RoleCache;
+
+import java.time.Duration;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class RoleRedisCache implements RoleCacheSource {
+    private final RedisTemplate<String, RoleCache> redisRoleTemplate;
+
+    @Override
+    public void set(String key, RoleCache value, Duration duration) {
+        redisRoleTemplate.opsForValue().set(key, value, duration);
+    }
+
+    @Override
+    public Optional<RoleCache> get(String key) {
+        return Optional.ofNullable(redisRoleTemplate.opsForValue().get(key));
+    }
+
+    @Override
+    public void delete(String key) {
+        redisRoleTemplate.delete(key);
+    }
+}
---
+++ src/main/java/ru/d2k/parkle/dao/database/role/RolePostgresDatabase.java
@@ -0,0 +1,56 @@
+package ru.d2k.parkle.dao.database.role;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.d2k.parkle.entity.Role;
+import ru.d2k.parkle.repository.RoleRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class RolePostgresDatabase implements RoleDatabaseSource {
+    private final RoleRepository roleRepository;
+
+    @Override
+    public Role save(Role entity) {
+        return roleRepository.save(entity);
+    }
+
+    @Override
+    public List<Role> getAll() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> getById(UUID id) {
+        return roleRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Role> getByName(String name) {
+        return roleRepository.findByName(name);
+    }
+
+    @Override
+    public Role getReferenceById(UUID id) {
+        return roleRepository.getReferenceById(id);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        roleRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return roleRepository.existsById(id);
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return roleRepository.existsByName(name);
+    }
+}
