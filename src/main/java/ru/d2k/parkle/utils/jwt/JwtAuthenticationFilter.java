3d71ddce6ac2e7372aa51f083d3f26584bc46b96
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-02-12 23:33:37 +0700

    refactor: rewrite doFilter() and add shouldNotFilter() in JwtAuthenticationFilter

--- src/main/java/ru/d2k/parkle/utils/jwt/JwtAuthenticationFilter.java
+++ src/main/java/ru/d2k/parkle/utils/jwt/JwtAuthenticationFilter.java
@@ -2,21 +2,23 @@ package ru.d2k.parkle.utils.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import ru.d2k.parkle.exception.JwtNotExistInRequestException;
-import ru.d2k.parkle.exception.JwtNotIncludeUserLoginException;
+import ru.d2k.parkle.service.security.cookie.CookieNames;
+import ru.d2k.parkle.service.security.cookie.CustomCookieService;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -24,7 +26,9 @@ import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final CustomCookieService cookieService;
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
 
@@ -35,53 +39,77 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException
     {
-        Optional<String> jwtOptional = JwtUtil.extractJwtFromCookie(request);
+        log.info("Request by path: {}", request.getContextPath() + request.getServletPath());
+
+        Optional<Cookie> cookie = cookieService.fetchCookie(CookieNames.JwtToken, request);
+
+        if (cookie.isEmpty()) {
+            log.warn("Cookie is empty. Do Filter.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Optional<String> jwtOptional = cookieService.getValueFromCookie(cookie.get());
 
         if (jwtOptional.isEmpty()) {
+            log.warn("JWT is empty. Do Filter.");
             filterChain.doFilter(request, response);
             return;
         }
 
         try {
-            final String jwt = jwtOptional
-                    .orElseThrow(() ->
-                            new JwtNotExistInRequestException("JWT token is null but extracting itself continued")
-                );
+            if (jwtOptional.get() instanceof String jwt) {
 
-            // TODO: Переделать. Одна и та же исключительная ситуация (как выше) делает разные ошибки.
-            // Заменить jwtOptional -> jwt.
-            final String userLogin = jwtUtil.extractUsername(jwtOptional
-                    .orElseThrow(() ->
-                            new JwtNotIncludeUserLoginException("JWT token is null but extracting login from itself continued")
-                    )
-                );
+                if (jwt.isBlank()) {
+                    log.warn("JWT is blank. Do Filter.");
+                    filterChain.doFilter(request, response);
+                    return;
+                }
 
-            if (
-                    Objects.nonNull(userLogin) &&
-                            Objects.isNull(SecurityContextHolder.getContext().getAuthentication())
-            ) {
-                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userLogin);
+                String userLogin = jwtUtil.extractUsername(jwt);
 
-                if (jwtUtil.isTokenValid(jwt, userDetails)) {
-                    UsernamePasswordAuthenticationToken authToken =
-                            new UsernamePasswordAuthenticationToken(
-                                    userDetails,
-                                    null,
-                                    userDetails.getAuthorities()
-                            );
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                if (
+                        Objects.nonNull(userLogin) &&
+                                Objects.isNull(SecurityContextHolder.getContext().getAuthentication())
+                ) {
+                    UserDetails userDetails = this.userDetailsService.loadUserByUsername(userLogin);
+
+                    if (jwtUtil.isTokenValid(jwt, userDetails)) {
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
                 }
             }
         }
         catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            log.error("Exception when JWT filter authentication process: {}", ex);
 
-            ResponseCookie responseCookie = jwtUtil.createJwtExpiredCookie();
+            ResponseCookie responseCookie = cookieService.createEmptyResponseCookie(
+                    CookieNames.JwtToken,
+                    true,
+                    false,
+                    "/",
+                    "Lax"
+            );
             response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
         }
         finally {
+            log.warn("At end. Do Filter.");
             filterChain.doFilter(request, response);
         }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+
+        return path.equals("/api/auth/login") ||
+                path.equals("/api/auth/registration") ||
+                path.equals("/api/auth/ping");
     }
 }
