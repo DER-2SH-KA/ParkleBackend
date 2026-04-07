1fe9eb49ddb394cf6315fee0f5f993f1b851d408
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-04-07 23:47:57 +0700

    feat: add static method for start logs to main application class

--- src/main/java/ru/d2k/parkle/ParkleProjectJun2025Application.java
+++ src/main/java/ru/d2k/parkle/ParkleProjectJun2025Application.java
@@ -8,6 +8,12 @@ public class ParkleProjectJun2025Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkleProjectJun2025Application.class, args);
+
+		startLogs();
+	}
+
+	private static void startLogs() {
+		System.out.println("App was started!");
 	}
 
 }
