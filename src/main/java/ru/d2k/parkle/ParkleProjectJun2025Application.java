cd4a597dd162404bdbd59f22dec0756aaa1f2e94
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-07-25 21:30:52 +0700

    refactor: configs and move EnableWebSecurity into Application class

--- src/main/java/ru/d2k/parkle/ParkleProjectJun2025Application.java
+++ src/main/java/ru/d2k/parkle/ParkleProjectJun2025Application.java
@@ -2,7 +2,9 @@ package ru.d2k.parkle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 @SpringBootApplication
 public class ParkleProjectJun2025Application {
 
