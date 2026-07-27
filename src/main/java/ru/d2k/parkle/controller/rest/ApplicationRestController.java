6ca3c319e2c379284609f33eab5076421b6d9cd7
DER-2SH-KA <KurumiForWeb@yandex.ru>
2026-07-27 21:28:13 +0700

    fix: error 401 '/api/ping' 'cause SecurityConfig settings

--- src/main/java/ru/d2k/parkle/controller/rest/ApplicationRestController.java
+++ src/main/java/ru/d2k/parkle/controller/rest/ApplicationRestController.java
@@ -5,10 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.d2k.parkle.controller.ApiRoutes;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping
+@RequestMapping(ApiRoutes.API)
 public class ApplicationRestController {
 
     @GetMapping("/ping")
